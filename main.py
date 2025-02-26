c22479183564c6ffe56c198e1840f2ef99c5459d
Fraanas <franek.biskup@pakunova.pl>
2025-02-26 16:06:56 +0100

    1st classes, modifications

--- main.py
+++ main.py
@@ -1,15 +1,61 @@
 from datetime import datetime
+date = datetime.now().year
+ADULT = 18
+
+def exercise_1(name, surname, born):
+    return print(f"Hello, {name} {surname}, born in {born}. \n")
+
+
+def exercise_2(name, surname):
+    name = name.upper()
+    surname = surname.upper()
+    return print(f"Hello, {name} {surname}. \nYour name has {len(name)} letter and your surname has {len(surname)} letters.\n")
+
+
+def exercise_3(name, surname, born):
+    age = date - born
+    return print(f"Hello, {name} {surname}, {age} yo.\n")
+
+
+def exercise_4(name, surname):
+    gender = 'female' if name[-1] == 'a' else 'male'
+    return print(f"Hello, {name} {surname}, I can assume that you are a {gender}.\n")
+
+def exercise_5(born):
+    age = date - born
+    adulthood = abs(age - ADULT)
+    if age < 18:
+        print(f"You are child. \nYou'll be an adult in {adulthood} years")
+    elif age > 18:
+        print(f"You are adult since {adulthood} years")
+    else:
+        print(f"You are an adult since this year")
+
 
 if __name__ == '__main__':
+    #exercise 6
+    data = input('Enter your name and surname: ')
+    name, surname = data.split(' ', 1)
+    #name = str(input("Enter your name: "))
+    #surname = str(input("Enter your surname: "))
+    born = int(input("Enter your year of born: "))
+
+    exercise_1(name, surname, born)
+    exercise_2(name, surname)
+    exercise_3(name, surname, born)
+    exercise_4(name, surname)
+    exercise_5(born)
+
+
+    '''
     ADULT = 18
     date = datetime.now().year
 
     name = str(input("Enter your name: "))
     surname = str(input("Enter your surname: "))
     born = int(input("Enter your year of born: "))
-    len_of_surname = len(surname)
     age = date - born
-    print(f"Hello, {name} {surname.upper()}, {age} yo, lenght of surname is {surname.__len__()}")
+    print(f"Hello, {name.upper()} {surname.upper()}, {age} yo. \n Length of surname is {name.__len__()}\n Length of surname is {surname.__len__()}")
 
     print("Female" if name[-1] == 'a'else 'Male')
 
@@ -19,3 +65,6 @@ if __name__ == '__main__':
     elif age > 18:
         print(f"You are adult since {adulthood} years")
     else: print(f"You are an adult since this year")
+'''
+
+
